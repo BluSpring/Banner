c63e4f991b63b0bcb1408d37b679f10e986f68c9
Naz <bluspringindohq@gmail.com>
2024-10-20 18:01:28 +0800

    fix some compile issues

--- banner-server/src/main/java/com/mohistmc/banner/bukkit/BukkitCauldronHooks.java
+++ banner-server/src/main/java/com/mohistmc/banner/bukkit/BukkitCauldronHooks.java
@@ -64,9 +64,4 @@ public class BukkitCauldronHooks {
         }
     }
 
-    public static boolean lowerFillLevel(BlockState iblockdata, Level world, BlockPos blockposition, Entity entity, CauldronLevelChangeEvent.ChangeReason reason) {
-        int i = iblockdata.getValue(LayeredCauldronBlock.LEVEL) - 1;
-        BlockState iblockdata1 = i == 0 ? Blocks.CAULDRON.defaultBlockState() : iblockdata.setValue(LayeredCauldronBlock.LEVEL, i);
-        return changeLevel(world, blockposition, iblockdata1, entity, reason);
-    }
 }
