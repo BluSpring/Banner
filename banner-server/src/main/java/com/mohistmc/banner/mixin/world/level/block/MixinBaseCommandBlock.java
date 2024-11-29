d29df0594626f83d412ed1e91516513ef3da7238
Naz <bluspringindohq@gmail.com>
2024-11-29 17:46:48 +0800

    Try to reduce the high memory usage caused by repeating command blocks

--- banner-server/src/main/java/com/mohistmc/banner/mixin/world/level/block/MixinBaseCommandBlock.java
+++ banner-server/src/main/java/com/mohistmc/banner/mixin/world/level/block/MixinBaseCommandBlock.java
@@ -1,22 +1,20 @@
 package com.mohistmc.banner.mixin.world.level.block;
 
-import com.google.common.base.Joiner;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.world.level.BaseCommandBlock;
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.event.server.ServerCommandEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BaseCommandBlock.class)
 public abstract class MixinBaseCommandBlock {
 
-    @Redirect(method = "performCommand", at = @At(value = "INVOKE", target = "Lnet/minecraft/commands/Commands;performPrefixedCommand(Lnet/minecraft/commands/CommandSourceStack;Ljava/lang/String;)V"))
-    private void banner$serverCommand(Commands commands, CommandSourceStack sender, String command) {
-        Joiner joiner = Joiner.on(" ");
+    @WrapOperation(method = "performCommand", at = @At(value = "INVOKE", target = "Lnet/minecraft/commands/Commands;performPrefixedCommand(Lnet/minecraft/commands/CommandSourceStack;Ljava/lang/String;)V"))
+    private void banner$serverCommand(Commands commands, CommandSourceStack sender, String command, Operation<Void> original) {
         if (command.startsWith("/")) {
             command = command.substring(1);
         }
@@ -28,7 +26,7 @@ public abstract class MixinBaseCommandBlock {
         }
         command = event.getCommand();
 
-        String[] args = command.split(" ");
+        /*String[] args = command.split(" ");
 
         String cmd = args[0];
         if (cmd.startsWith("minecraft:")) cmd = cmd.substring("minecraft:".length());
@@ -42,8 +40,8 @@ public abstract class MixinBaseCommandBlock {
 
         if (((CraftServer) Bukkit.getServer()).getCommandBlockOverride(args[0])) {
             args[0] = "minecraft:" + args[0];
-        }
+        }*/
 
-        commands.performPrefixedCommand(sender, joiner.join(args));
+        original.call(commands, sender, command);
     }
 }
